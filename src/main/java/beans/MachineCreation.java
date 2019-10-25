58cc660e49345cd932bc896d2d6121000b63547d
taciturnn <taciturnn@gmail.com>
2019-10-25 13:23:55 -0400

    Updated machine xhtml.

--- src/main/java/beans/MachineCreation.java
+++ src/main/java/beans/MachineCreation.java
@@ -20,7 +20,15 @@ public class MachineCreation extends Machine implements Serializable {
 
     public String execute() {
         machineManager.createMachine(this);
+        this.ResetMachine();
         return navigationController.goToMachines();
     }
 
+    private void ResetMachine()
+    {
+        this.setId(0);
+        this.setFamily("");
+        this.setModel("");
+        this.setRentPrice(0);
+    }
 }
