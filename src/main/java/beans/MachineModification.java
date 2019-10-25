70f909fb34f88463bec9e6292b3c02ba9d65412c
taciturnn <taciturnn@gmail.com>
2019-10-25 15:46:07 -0400

    Added edit page for the machines.

---
+++ src/main/java/beans/MachineModification.java
@@ -0,0 +1,48 @@
+package beans;
+
+import beans.common.NavigationController;
+import beans.entities.Machine;
+import managers.MachineManagerInterface;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class MachineModification extends Machine implements Serializable {
+
+    @Inject
+    private MachineManagerInterface machineManager;
+    @Inject
+    private NavigationController navigationController;
+
+    public String Edit() {
+        machineManager.editMachine(this);
+        return navigationController.goToMachines();
+    }
+
+    public String Delete() {
+        machineManager.deleteMachine(this);
+        return navigationController.goToMachines();
+    }
+
+    public String edit(int id) {
+        try {
+            this.Clone(machineManager.findMachineById(id));
+            return navigationController.goToMachineModification();
+        } catch (Exception e) {
+            return navigationController.goToMachines();
+        }
+    }
+
+    public void Clone(Machine machine)
+    {
+        this.setFamily(machine.getFamily());
+        this.setId(machine.getId());
+        this.setModel(machine.getModel());
+        this.setRentPrice(machine.getRentPrice());
+    }
+
+}
