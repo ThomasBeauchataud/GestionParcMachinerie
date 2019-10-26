f0a43bac3b62bbbfb310034f063b5cd7b41f3a07
Q5h3gf <sam.android.sg@gmail.com>
2019-10-26 03:10:09 -0400

    created ClientsView

---
+++ src/main/java/beans/views/ClientsView.java
@@ -0,0 +1,35 @@
+package beans.views;
+
+import beans.entities.Client;
+import managers.ClientManagerInterface;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+@Named
+@RequestScoped
+public class ClientsView implements Serializable {
+
+    @Inject
+    private ClientManagerInterface clientManager;
+    private List<Client> clientList;
+
+    @PostConstruct
+    public void init() {
+        this.clientList = clientManager.findAllClients();
+    }
+
+    public List<Client> getClientList() {
+        return clientList;
+    }
+
+    public void setClientList(List<Client> clientList) {
+        this.clientList = clientList;
+    }
+
+}
